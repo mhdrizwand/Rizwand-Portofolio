3ce1f946e6beb951617bd29471b5c7c87782a96b
nama kamu <muhammadrizwand001@gmail.com>
2026-04-03 09:52:23 +0700

    update kantah 2

--- components/certificate-section.tsx
+++ components/certificate-section.tsx
@@ -85,7 +85,7 @@ const certificates = [
     category: "lainnya",
     date: "2024-2025",
     badge: "Certified",
-    img: "/images/sertifikat/ST_lainnya.png",
+    img: "/images/sertifikat/ST_banyak.png",
   },
 ];
 
